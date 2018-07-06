ce41d0ce3dc0cb42645a3fe6607eb1ac2525faa0
Andy Lowry <andy@andylowry.me.uk>
2018-07-06 17:00:13 +0100

    First working test

--- responder/src/main.rs
+++ responder/src/main.rs
@@ -1,3 +1,19 @@
+extern crate env_logger;
+extern crate mdns_responder;
+extern crate dns_parser;
+
 fn main() {
-    println!("Hello, world!");
+    
+    env_logger::init();
+
+    let responder = mdns_responder::Responder::new().unwrap();
+    let _svc = responder.register(
+        "_devpolterg._tcp".to_owned(),
+        "deadbeefdeadbeef".to_owned(),
+        8883,
+        &["instance=dev", "id=deadbeefdeadbeef"]);
+
+    loop {
+        ::std::thread::sleep(::std::time::Duration::from_secs(10));
+    }
 }
